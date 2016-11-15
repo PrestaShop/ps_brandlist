3e66aa5aa8e7f3f1df37a8b42662086d9ac2d6e5
Maxime Biloé <maxime.biloe@prestashop.com>
2016-11-15 11:31:22 +0100

    // Version updated to v1.0.1

--- ps_brandlist.php
+++ ps_brandlist.php
@@ -38,7 +38,7 @@ class Ps_Brandlist extends Module implements WidgetInterface
     {
         $this->name = 'ps_brandlist';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
